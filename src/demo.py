7694d772e86f939d0ba27e9c303f052a4b6e8180
Svetlin <svetlimladenov@gmail.com>
2021-09-29 18:33:19 +0300

    First Demo

--- src/demo.py
+++ src/demo.py
@@ -1,1 +1,13 @@
-from PIL import Image
+import wx;
+
+app = wx.App(clearSigInt=True)
+
+frame = wx.Frame(parent=None, id=wx.ID_ANY, title="My First App", pos= (100, 100))
+
+panel = wx.Panel(parent=frame, id=wx.ID_ANY)
+
+welcomeText = wx.StaticText(parent=panel, label="Hello World!!!", pos = (20, 20))
+
+frame.Show()
+
+app.MainLoop()
