4a4293026d2e45c1538ae7ee64ac4c7138f8994c
Svetlin <svetlimladenov@gmail.com>
2021-09-29 20:05:58 +0300

    Make some parsing

--- src/demo.py
+++ src/demo.py
@@ -26,25 +26,33 @@ class MyPanel(wx.Panel):
         super().__init__(parent=parent)
 
         # add a hello message to the panel
-        wx.StaticText(self, id=wx.ID_ANY, label="Type character name: ", pos = (20, 20))
+        wx.StaticText(self, id=wx.ID_ANY, label="Type Artist name: ", pos = (20, 20))
         # ID_ANY mean thath we don't care about the id
 
         self.inputBox = wx.TextCtrl(self, pos=(20, 50), size=(100, 20)) # set it as a field to the class 
-        self.result = wx.StaticText(self, id=wx.ID_ANY, label="Result will be displayed here", pos=(20, 80))
+        self.result = wx.StaticText(self, id=wx.ID_ANY, label="Data will be displayed here", pos=(20, 80))
 
-        button = wx.Button(parent=self, label="Fetch Data", pos = (20, 110))
+        button = wx.Button(parent=self, label="Fetch Artist Data", pos = (20, 110))
         button.Bind(wx.EVT_BUTTON, self.onFetch)
     
     def onFetch(self, event):
-        # params = {"v": "LosIGgon_KM", "t": "5m55s"}
-        # queryString = parse.urlencode(params) # returns string suitable for a query string
-        baseUrl = "https://swapi.dev/api/people/"
         input = self.inputBox.GetValue()
-        url = baseUrl + input
+        params = {"query": f'artist:{input}'}
+        queryString = parse.urlencode(params)
+        url = f'http://musicbrainz.org/ws/2/artist/?{queryString}'
         response = request.urlopen(url)
         data = response.read() # returns as bytes 
         html = data.decode("UTF-8") # turn the bytes to string
-        self.result.SetLabel(html[:100])
+        artistName = self.getArtistName(html, "name")
+        self.result.SetLabel(artistName)
+
+    def getArtistName(self, html, property):
+        print(html)
+        left = html.find(f'<{property}>')
+        right = html.find(f'</{property}>')
+        artistName = html[left + len(property) + 2:right]
+        return artistName
+        
 
 if __name__ == "__main__":
     app = MyApp("F97160")
