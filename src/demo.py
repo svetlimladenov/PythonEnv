ec4c7925376591c0747f6d578db00830bec4ed14
Svetlin <svetlimladenov@gmail.com>
2021-09-29 19:44:54 +0300

    Display the json response to the ui

--- src/demo.py
+++ src/demo.py
@@ -1,18 +1,20 @@
 import wx;
+from urllib import request
+from urllib import parse # use to generate the query params
 
-class MyApp(wx.App):
-    def __init__(self):
-        super().__init__(clearSigInt=True)
+class MyApp(wx.App): # MyApp : wx.App - inheritance
+    def __init__(self, title):
+        super().__init__(clearSigInt=True) # call the wx.App constructor
 
         # init frame
-        self.InitFrame()
+        self.InitFrame(title) # self -> this
 
-    def InitFrame(self):
-        frame = MyFrame(parent=None, title="My Frame", pos= (100, 100))
+    def InitFrame(self, title):
+        frame = MyFrame(parent=None, title=title, pos= (200, 200))
         frame.Show()
 
 class MyFrame(wx.Frame):
-    def __init__(self, parent, title, pos):
+    def __init__(self, parent, title, pos): #constructor (parent, title, pos)
         super().__init__(parent=parent, title=title, pos=pos)
         self.OnInit()
     
@@ -24,10 +26,26 @@ class MyPanel(wx.Panel):
         super().__init__(parent=parent)
 
         # add a hello message to the panel
-        welcomeText = wx.StaticText(self, id=wx.ID_ANY, label="Hello World!!!", pos = (20, 20))
+        wx.StaticText(self, id=wx.ID_ANY, label="Type character name: ", pos = (20, 20))
         # ID_ANY mean thath we don't care about the id
 
+        self.inputBox = wx.TextCtrl(self, pos=(20, 50), size=(100, 20)) # set it as a field to the class 
+        self.result = wx.StaticText(self, id=wx.ID_ANY, label="Result will be displayed here", pos=(20, 80))
+
+        button = wx.Button(parent=self, label="Fetch Data", pos = (20, 110))
+        button.Bind(wx.EVT_BUTTON, self.onFetch)
+    
+    def onFetch(self, event):
+        # params = {"v": "LosIGgon_KM", "t": "5m55s"}
+        # queryString = parse.urlencode(params) # returns string suitable for a query string
+        baseUrl = "https://swapi.dev/api/people/"
+        input = self.inputBox.GetValue()
+        url = baseUrl + input
+        response = request.urlopen(url)
+        data = response.read() # returns as bytes 
+        html = data.decode("UTF-8") # turn the bytes to string
+        self.result.SetLabel(html[:100])
 
 if __name__ == "__main__":
-    app = MyApp()
+    app = MyApp("F97160")
     app.MainLoop()
